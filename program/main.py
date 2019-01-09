e117a3132803adfd9063bf87dcf380c01657fb28
Tuan Trieu <tat2016@mac178169.local>
2019-01-09 18:29:52 -0500

    refactor main.py

--- program/main.py
+++ program/main.py
@@ -35,7 +35,7 @@ import h5py
 import numpy as np
 
 from keras.utils import multi_gpu_model
-import sys
+
 
 # import variant_class
 import boundary_class
@@ -44,59 +44,51 @@ import common_data_generation as cdg
 
 import argparse
 
-parser = argparse.ArgumentParser(description='Process some integers.')
-parser.add_argument('-ssm', dest='ssm', default="", help='simple somatic mutation file')
-parser.add_argument('-sv', dest='sv', default="", help='structural variant file')
-
-opts = parser.parse_args()
-
-# retrieve mutations file name from arguments
-inputF = 'tsv' #format file
-ssmFileName = opts.ssm
-svFileName = opts.sv
 
 
+ref_genome_file = 'Homo_sapiens.GRCh37.75.dna.primary_assembly.fa'
+cons_loop_file = 'loopDB/constitutive_loops.xlsx'
 
 # os.environ["CUDA_VISIBLE_DEVICES"] = "4,5,6,7"
 loop_model_file = 'model/loop_pred_4k_1gpu.h5'
 ngpu = 1 # number of gpu to use, combine with os.environ above if necessary
 
+nbr_job = 1  # number of parallel jobs when creating data for patients
+
 # parameters to load data
 batch_size = 24 * ngpu
 
+# constants
 segment_size = 4000
 version = ''
 nbr_feature = 10
 rnn_len = 800
-params = {'dim': (segment_size, nbr_feature),
-          'batch_size': batch_size,
-          'n_channels': 1,
-          'rnn_len': rnn_len}
-
-jobID = str(int(time.time()))
-nbr_job = 1  # number of parallel jobs when creating data for patients
-
-ref_genome_file = 'Homo_sapiens.GRCh37.75.dna.primary_assembly.fa'
-# data file for each patient
-consitutive_loop_data_sampleid = lambda x: '%s/%s_loop_%s.mat' % (jobID, x, jobID)
-
-# generate names and create output folders
-if not os.path.exists(jobID):
-    os.makedirs(jobID)
-
-outputFolder = str(jobID) + '_output'
-inputFolder = str(jobID)
-if not os.path.exists(outputFolder):
-    os.makedirs(outputFolder)
-
-loopFile = 'loopDB/constitutive_loops.xlsx'
 
 
+
+def makeOutputFolder():
+    jobID = str(int(time.time()))
+
+    # generate names and create output folders
+    if not os.path.exists(jobID):
+        os.makedirs(jobID)
+
+    outputFolder = str(jobID) + '_output'
+    inputFolder = str(jobID)
+    if not os.path.exists(outputFolder):
+        os.makedirs(outputFolder)
+
+    return(jobID, inputFolder, outputFolder)
 
 
 ###########################################################
 def get_prediction(model_file, data_file, isloop=True):
     """Run prediction for a data file"""
+
+    params = {'dim': (segment_size, nbr_feature),
+              'batch_size': batch_size,
+              'n_channels': 1,
+              'rnn_len': rnn_len}
 
     if isinstance(model_file, str):
         model = load_model(model_file)
@@ -132,7 +124,7 @@ def output_prob(rs, file_name):
     fout.close()
 
 
-def process_patient(loops, varlist, segment_size, ref_genome_file, output_file):
+def process_eachpatient(loops, varlist, segment_size, ref_genome_file, output_file):
     """Generate data file for each patient
 
     """
@@ -147,66 +139,111 @@ def process_patient(loops, varlist, segment_size, ref_genome_file, output_file):
             cdg.output_loop(looplist, ref_genome_file, segment_size, output_file, None)
 
 
-# variants = variant_class.extract_variants(fileName, '', all_variant = True)
+def generate_patientdata(variants, loops, nbr_job, jobID):
+    print('Creating input data for all patients...')
+
+    # data file for each patient
+    consitutive_loop_data_sampleid = lambda x: '%s/%s_loop_%s.mat' % (jobID, x, jobID)
+
+    if nbr_job > 1:
+        pool = mp.Pool(processes=nbr_job)
+        result = [pool.apply_async(process_eachpatient, args=(loops, variants[k], segment_size,
+                                                          ref_genome_file, consitutive_loop_data_sampleid(k))) for k in
+                  variants]
+        pool.close()
+        pool.join()
+
+    else:
+        for k in variants:
+            process_eachpatient(loops, variants[k], segment_size, ref_genome_file, consitutive_loop_data_sampleid(k))
+
+    print('Done preparing data')
+
+
+def makePrediction(loop_model_file, inputFolder, outputFolder):
+
+    print('Loading model')
+    model = load_model(loop_model_file)
+    if ngpu > 1:
+        model = multi_gpu_model(model, gpus=ngpu)
+
+    patient_files = os.listdir(inputFolder)
+
+    print('Running model on input data')
+    for i in patient_files:
+
+        input_file = os.path.join(inputFolder, i)
+
+        dataset = h5py.File(input_file, 'r')
+
+        print(input_file)
+        if len(dataset.items()) == 0:
+            continue
+
+        output_file = os.path.join(outputFolder, i.replace('.mat', '.txt'))
+
+        rs = get_prediction(model, input_file)
+
+        output_prob(rs, output_file)
 
 ############################## Generating data
-ssmVariants = {}
-svVariants = {}
 
-print('Reading variants ...')
+def main():
 
-if ssmFileName:
-    ssmVariants = cdg.getSSM(ssmFileName)
-if svFileName:
-    svVariants = cdg.getSV(svFileName)
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('-ssm', dest='ssm', default="", help='simple somatic mutation file')
+    parser.add_argument('-sv', dest='sv', default="", help='structural variant file')
+    parser.add_argument('-loop', dest='loop_file', default = cons_loop_file, help='loop file')
 
-variants = {**ssmVariants, **svVariants}
+    opts = parser.parse_args()
 
-if '.xlsx' in loopFile:
-    loops = cdg.getLoopXLSX(loopFile)
-else:
-    loops = boundary_class.read_loop(loopFile)
+    # retrieve mutations file name from arguments
+    inputF = 'tsv' #format file
+    ssmFileName = opts.ssm
+    svFileName = opts.sv
 
-print('Creating input data ...')
+    loop_file = opts.loop_file
 
-if nbr_job > 1:
-    pool = mp.Pool(processes=nbr_job)
-    result = [pool.apply_async(process_patient, args=(loops, variants[k], segment_size,
-                                                      ref_genome_file, consitutive_loop_data_sampleid(k))) for k in
-              variants]
-    pool.close()
-    pool.join()
+    ssmVariants = {}
+    svVariants = {}
 
-else:
-    for k in variants:
-        process_patient(loops, variants[k], segment_size, ref_genome_file, consitutive_loop_data_sampleid(k))
+    print('Reading variants ...')
 
-print('Done preparing data')
+    if ssmFileName:
+        ssmVariants = cdg.getSSM(ssmFileName)
+    if svFileName:
+        svVariants = cdg.getSV(svFileName)
 
-''' '''
+    variants = {**ssmVariants, **svVariants}
 
-############################### Running the model for all patients
+    if '.xlsx' in loop_file:
+        loops = cdg.getLoopXLSX(loop_file)
+    else:
+        loops = boundary_class.read_loop(loop_file)
 
-print('Loading model')
-model = load_model(loop_model_file)
-if ngpu > 1:
-    model = multi_gpu_model(model, gpus=ngpu)
 
-patient_files = os.listdir(inputFolder)
 
-print('Running model on input data')
-for i in patient_files:
+    #calculate probability for loops without mutations
+    if not ssmFileName and not svFileName:
+        print('No variants found, calculating loop probability for loops ...')
 
-    input_file = os.path.join(inputFolder, i)
+        output_seq_file = loop_file.replace(".xlsx", ".mat")
+        [loops, _] = cdg.get_loop(loops, [], segment_size, isNormalize=False, isNonLoop=False, noLargeSV=False)
+        cdg.output_loop(loops, ref_genome_file, segment_size, output_seq_file, None)
 
-    dataset = h5py.File(input_file, 'r')
+        rs = get_prediction(loop_model_file, output_seq_file)
 
-    print(input_file)
-    if len(dataset.items()) == 0:
-        continue
+        output_prob(rs, output_seq_file.replace(".mat", "_probability.txt"))
 
-    output_file = os.path.join(outputFolder, i.replace('.mat', '.txt'))
+        os.remove(output_seq_file)
 
-    rs = get_prediction(model, input_file)
+        return
 
-    output_prob(rs, output_file)
+    jobID, inputDataFolder, outputFolder = makeOutputFolder()
+
+    generate_patientdata(variants, loops, nbr_job, jobID)
+
+    makePrediction(loop_model_file, inputDataFolder, outputFolder)
+
+
+main()
